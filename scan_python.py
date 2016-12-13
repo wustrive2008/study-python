c72a3c701a31c2ccd71636caf1c4c140aac719d6
wubaoguo <wustrive_2008@126.com>
2016-12-14 00:23:12 +0800

    优化

--- scan_python.py
+++ scan_python.py
@@ -11,8 +11,9 @@ def print_pdf (root, dirs, files):
     for file in files:
         path = os.path.join(root, file)
         path = os.path.normcase(path)
-        if re.search(r".*\.pdf", path):
-            print(path)
+        if not re.search(r".*\.pdf", path): continue
+        if re.search(r" ", path): continue
+        print(path)
 
 for root, dirs, files in os.walk('/Users/wubaoguo/Documents/pdf文档'):
     print_pdf(root, dirs, files)
