106645be7c2b21f96f9ec69224ebeab8529fb791
wubaoguo <wustrive_2008@126.com>
2016-12-30 00:02:52 +0800

    添加文件qiushiCrawler

---
+++ qiushiCrawler.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+#爬取糗事百科段子
+
+import urllib
+import urllib2
+import re
+
+page = 1
+url = 'http://www.qiushibaike.com/hot/page/' + str(page)
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
+try:
+    request = urllib2.Request(url,headers = headers)
+    response = urllib2.urlopen(request)
+    content = response.read().decode('utf-8')
+    #print content
+    pattern = re.compile('<h2>(.*?)</h2>.*?<div class="content">\s+<span>(.*?)</span>\s+</div>',re.S)
+    items = re.findall(pattern,content)
+    #print items
+    for item in items:
+        print '作者:',item[0],'\n','内容:',re.sub(r'<br[ ]?/?>', '\n', item[1])
+        print " "
+except urllib2.URLError, e:
+    if hasattr(e,"code"):
+        print e.code
+    if hasattr(e,"reason"):
+        print e.reason
